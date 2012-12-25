5d15f872105e465e3a7d96e502e340951d7da1c1
Alexander <korenev.alexander@gmail.com>
2012-12-26 01:29:31 +0400

    сделал верстку страницы

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+        echo '<html>';
+        echo '<head>';
+        echo '<title>Заголовок страницы</title>';
+        echo '</head>';
+        echo '<body>';
+        echo '<table border="1" cellpadding="0" cellspacing="0" width="100%" height="100%">';
+        echo '<tr>  <th colspan=2>header</th>  </tr>';
+        echo '<tr>
+                <td width="25%" height="100%" valign="top">
+                <ul>
+                        <li><a href="index.html" title="Ссылка">link</a></li>
+                        <li><a href="index.html" title="Ссылка">link</a></li>
+                        <li><a href="index.html" title="Ссылка">link</a></li>
+                </ul>
+                </td>
+                <td width="75%" height="100%" valign="top">content</td>
+              </tr>';
+        echo '<tr> <td colspan=3>footer</td> </tr>';
+        echo '</table>';
+?>
+
