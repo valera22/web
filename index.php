94fba93022672e188e877f50068ddb5d4f072510
Alexander <korenev.alexander@gmail.com>
2013-01-20 20:01:10 +0400

    подключил таблицу стилей

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
         echo '<html>';
         echo '<head>';
         echo '<title>Заголовок страницы</title>';
+	echo '<link rel="stylesheet" type="text/css" href="style.css">';
         echo '</head>';
         echo '<body>';
         echo '<table border="1" cellpadding="0" cellspacing="0" width="100%" height="100%">';
